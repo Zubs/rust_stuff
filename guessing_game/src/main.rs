87280d27f399380a100ce5ce51f057d7d710da41
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-01-11 11:12:51 +0100

    Fixed

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("Guessing Game");
     println!("Please input your guess");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Unable to get your input");
