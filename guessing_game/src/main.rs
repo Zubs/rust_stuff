fec372089bd03f5850e0b2826a278a16c8ef3eec
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-01-12 13:24:09 +0100

    Multiple attempts

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,19 +6,23 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guessing Game");
-    println!("Please input your guess");
-
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Unable to get your input");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    loop {
+        println!("Please input your guess");
 
-    println!("The secret number is: {}", secret_number);
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Unable to get your input");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("Perfect! You win!!!")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("The secret number is: {}", secret_number);
+                println!("Perfect! You win!!!");
+                break;
+            }
+        }
     }
 }
